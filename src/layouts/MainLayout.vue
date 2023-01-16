e1818612db32ebc04628949258033eaf7ea5ffd1
lapinRagnar <lapinragnar@gmail.com>
2023-01-16 22:49:37 +0100

    2 - page calculatrice

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -22,6 +22,7 @@
       v-model="leftDrawerOpen"
       show-if-above
       bordered
+      :width=200
     >
       <q-list>
         <q-item-label
@@ -29,6 +30,14 @@
         >
           Bonjour
         </q-item-label>
+
+        <q-item clickable v-ripple :to="{name: 'calculatrice'}">
+          <q-item-section avatar>
+            <q-avatar color="teal" text-color="white" icon="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Calculatrice</q-item-section>
+        </q-item>
 
       </q-list>
     </q-drawer>
@@ -41,65 +50,16 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
 
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
---
+++ src/pages/CalculatricePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page padding>
+    <h3>Ma Calculatrice</h3>
+  </q-page>
+</template>
+
+<script setup>
+
+
+</script>
